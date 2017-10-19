118f4d2998b4b122167e1e71cd45a5cccae4f7bc
Anton Neverov <webpuritycg@gmail.com>
2017-10-19 21:41:58 +0900

    fix webpack less

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
-let string1 = 'hello';
 let $ = require('jquery');
+require('./less');
+
 let App = function() {
-    console.log(string1);
-    $('body').html(string1);
+    $('body').html('<div class="b-test"><h3 class="b-test__heading">Heading</h3></div>');
 };
 let app = new App();
 app.fuck = function() {
