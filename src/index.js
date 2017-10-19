54f7832fcca7c88dd528b6f7a5905668d3f483d7
Anton Neverov <webpuritycg@gmail.com>
2017-10-19 21:07:40 +0900

    eslint js es6 support add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 
 let string1 = 'hello';
 let $ = require('jquery');
-let App = function () {
+let App = function() {
     console.log(string1);
     $('body').html(string1);
 };
 let app = new App();
+app.fuck = function() {
+    console.log('hello');
+};
